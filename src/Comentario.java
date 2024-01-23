1d2f34054cc6dcf598a78785f682659b6b7ebffe
Alysson <alyssos@hotmail.com>
2024-01-23 15:13:43 -0300

    :tada: iniciando java

---
+++ src/Comentario.java
@@ -0,0 +1,16 @@
+/**
+ * JAVADOC - comentário voltado à documentação do arquivo
+ */
+
+public class Comentario {
+    public static void main(String[] args) {
+        // comentário somente em linha
+        System.out.println("oi");
+    }
+}
+/*
+*
+*   Comentário em blocos
+*
+*
+* */
