0afddac96a4933b590803a9bc0212a7a9dcc0d69
Alysson <alyssos@hotmail.com>
2024-01-23 15:48:37 -0300

    ✨ feat: Dados por referência e tipos de dados primitivos

---
+++ src/Dados.java
@@ -0,0 +1,27 @@
+public class Dados {
+    public static void main(String[] args) {
+        // Tipos por referência
+        Byte oByte = 5;
+        Short oShort = 5;
+        Integer oInterager = 5;
+        Long oLong = 10L;
+        Float oFloat = 10.0f;
+        Double oDouble = 10.0;
+        Boolean oBoolean = true;
+        String str = "Teste";
+        Character c = 'c';
+
+        //Tipos prímitivos
+        byte aByte = 5;
+        short aShort = 5;
+        int aInt = 5;
+        long aLong = 10L;
+        float aFloat = 10.0f;
+        double aDouble = 10.0;
+        boolean aBoolean = true;
+        char b = 'b';
+
+        System.out.println(oByte.toString());
+        //System.out.println(aByte.toString);
+    }
+}
