f8606795bf9535151bcf0c05ef4c3f8a8f777d6c
Alysson <alyssos@hotmail.com>
2024-01-23 15:37:42 -0300

    ✨ feat: Variáveis e suas propriedades

---
+++ src/variaveis.java
@@ -0,0 +1,23 @@
+public class variaveis {
+    public static void main(String[] args) {
+        String nome = "Alysson";
+        System.out.println("Minimo Byte: "+ Byte.MIN_VALUE + " e Máximo Byte: "+ Byte.MAX_VALUE);
+        System.out.println("Minimo shorte: "+ Short.MIN_VALUE + " e Máximo short: "+ Short.MAX_VALUE);
+        System.out.println("Minimo Integer: "+ Integer.MIN_VALUE + " e Máximo Integer: "+ Integer.MAX_VALUE);
+        System.out.println("Minimo Long: "+ Long.MIN_VALUE + " e Máximo Long: "+ Long.MAX_VALUE);
+    }
+}
+/**
+ * TIPO     Bit
+ * Double   64
+ * Float    32
+ *
+ * Long     64
+ * Interger 32
+ * Short    16
+ * Byte     8
+ *
+ * Boolean  ?
+ * String   ?
+ * Character ?
+ * */
