f09eb213cefe1fc9811f90b88977a32dde3baa2a
Alysson <alyssos@hotmail.com>
2024-01-24 14:34:53 -0300

    ✨ feat: Precedência entre Operadores Matemáticos

--- src/Prioridades_OP.java
+++ src/Prioridades_OP.java
@@ -1,2 +1,16 @@
 public class Prioridades_OP {
+    public static void main(String[] args) {
+        // Precedência de sinais
+        // divisão /
+        // multiplicação *
+        // resto da divisão %
+        // adição e subtração
+        //int x = 10/2*3+4*3; // 5*3+4*3 // 15+12 = 27
+        int x = 10/2*(3+4)*3; // 10/2*7*3 // 5*7*3 = 35*3 = 105
+        System.out.println(x);
+
+        //int a = 6*3+2; //18+2 = 20
+        int a = 6*(3+2); //18+2 = 30
+        System.out.println(a);
+    }
 }
