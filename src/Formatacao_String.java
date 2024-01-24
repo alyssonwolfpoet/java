be9fc9932ac95b10393142bb5e715f278e6b0acb
Alysson <alyssos@hotmail.com>
2024-01-24 20:30:28 -0300

    ✨ feat: Formatação de Strings

---
+++ src/Formatacao_String.java
@@ -0,0 +1,22 @@
+public class Formatacao_String {
+    public static void main(String[] args) {
+        String a = "Olá,";
+        String b = "Felipe";
+        System.out.println(a+" "+b);
+
+        String c = "dadlkanhfaliufhlwfawfwaffa, f wf " +
+                "wf fw f,w fw e er" +
+                " e, be brt brt br, r br br , " +
+                "rtb t bb  ,btr";
+
+        System.out.println("Testando " +
+                "meu " +
+                "software " +
+                "\nde " +
+                "varias " +
+                "formas");
+
+        System.out.println("Ele me disse 'oi'.");
+        System.out.println("Ele me disse \"OI\".");
+    }
+}
