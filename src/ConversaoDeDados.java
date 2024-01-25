1246c8b58b05eb2d1d39506469b5f29e9e4e2440
Alysson <alyssos@hotmail.com>
2024-01-24 21:00:05 -0300

    ✨ feat: Conversão de Dados

---
+++ src/ConversaoDeDados.java
@@ -0,0 +1,21 @@
+public class ConversaoDeDados {
+    public static void main(String[] args) {
+        String str = "100";
+            System.out.println(Integer.valueOf(str)+200);
+            System.out.println(Byte.valueOf(str)+200);
+            System.out.println(Short.valueOf(str)+200);
+            System.out.println(Float.valueOf(str)+200);
+            System.out.println(Double.valueOf(str)+200);
+            System.out.println("______________________________________");
+        Byte b = 10;
+            System.out.println(b.intValue());
+            System.out.println(b.shortValue());
+            System.out.println(b.floatValue());
+            System.out.println(b.doubleValue());
+            System.out.println(b.toString());
+        System.out.println("______________________________________");
+        Double c = 1243.356456456568678678;
+        System.out.println(c.intValue());
+        System.out.println(c.floatValue());
+    }
+}
