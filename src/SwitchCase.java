3e37db0e7cc804f3d8c0c785409e3fafcd7acac1
Alysson <alyssos@hotmail.com>
2024-01-25 16:29:15 -0300

    ✨ feat:Switch Case

---
+++ src/SwitchCase.java
@@ -0,0 +1,39 @@
+public class SwitchCase {
+    float calculaBonus(String cargo,float salario){
+        float bonus = 0f;
+        cargo = cargo.toLowerCase();
+        switch (cargo){
+            case "gerente":
+                bonus = salario*1.5f;
+                break;
+            case "analista":
+                bonus = salario*1.3f;
+                break;
+            case "supervisor":
+                bonus = salario*1.1f;
+                break;
+            default:
+                bonus = salario*0.7f;
+                break;
+        }
+        return bonus;
+
+        /*if (cargo.equals("gerente")){
+            bonus = salario*1.5f;
+        }else if(cargo.equals("analista")){
+            bonus = salario * 1.3f;
+        }else if(cargo.equals("supervisor")){
+            bonus = salario * 1.1f;
+        }else {
+            bonus = salario * 0.7f;
+        }
+        return bonus;*/
+    }
+    public static void main(String[] args) {
+        String cargo = "gerente";
+        float salario = 1000f;
+        float bonus = new SwitchCase().calculaBonus(cargo,salario);
+        System.out.println("O salario do cargo "+cargo+" é: "+salario + "e sua bonificação é: "+ bonus);
+        System.out.println("seu ganho foi de: "+(salario+bonus));
+    }
+}
