ce3342baa9a0faded47db3349446430f69798dcf
Alysson <alyssos@hotmail.com>
2024-01-24 21:30:13 -0300

    ✨ feat: Funções String

---
+++ src/Formato_String.java
@@ -0,0 +1,19 @@
+public class Formato_String {
+    public static void main(String[] args) {
+        String str = "Curso de Android.";
+        System.out.println("Tamanho da String: "+str.length());
+        System.out.println("Terceira posição da String e: "+str.charAt(2));
+        System.out.println(str.equals("Curso de Android."));
+        System.out.println(str.startsWith("Curso"));
+        System.out.println(str.endsWith("Android."));
+        System.out.println(str.substring(5));
+        System.out.println(str.substring(0,5));
+        System.out.println(str.replace("Curso","Curso top"));
+        System.out.println(str.replace("o","x"));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        String str2 = "             Curso de Android 2                   ";
+        System.out.println(str2.trim());
+
+    }
+}
