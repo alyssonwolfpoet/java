af25c782a4b8f9b8f78923025d4b46d5ee644950
Alysson <alyssos@hotmail.com>
2024-01-24 21:37:25 -0300

    ✨ feat: Funções Matemáticas

---
+++ src/Math_Funcao.java
@@ -0,0 +1,13 @@
+public class Math_Funcao {
+    public static void main(String[] args) {
+        System.out.println(Math.max(10,20));
+        System.out.println(Math.min(10,20));
+        System.out.println(Math.sqrt(100.0));
+        System.out.println(Math.log10(100));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.round(2005.5699));
+        System.out.println(Math.random());
+    }
+}
