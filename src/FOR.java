b3804e379ba0f3281c5be302301d7b43c6674250
Alysson <alyssos@hotmail.com>
2024-01-25 16:46:46 -0300

    ✨ feat:Estrutura de repetição FOR

---
+++ src/FOR.java
@@ -0,0 +1,19 @@
+public class FOR {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {//inicio;condição de parada;forma de contar
+            System.out.print(i + " "); // i++ -> i+=1 -> i = i + 1
+        }
+        System.out.println();
+        for (int i = 9;i>=0;i--){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0;i<10;i+=2){ //contando de 2 em 2 de forma crescente
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 9;i>=0;i-=2){ //contando de 2 em 2 de forma decrescente
+            System.out.print(i + " ");
+        }
+    }
+}
