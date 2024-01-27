ce8392be9753f547e0e13c4ab1a040a564344f13
Alysson <alyssos@hotmail.com>
2024-01-27 16:30:23 -0300

    ✨ feat: Leitura de dados em vetores

--- src/JavaTratamentodeDados/Entrada_Vetores.java
+++ src/JavaTratamentodeDados/Entrada_Vetores.java
@@ -1,5 +1,22 @@
 package JavaTratamentodeDados;
 
-public class Entrada_Vetores {
+import java.util.Scanner;
 
+public class Entrada_Vetores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o tamanho do vetor: ");
+        int tamanho = sc.nextInt();
+
+        int[] vetor = new int[tamanho];
+        System.out.println("DIgite os elementos do vetor: ");
+        for (int i = 0; i < tamanho; i++){
+            vetor[i] = sc.nextInt();
+        }
+        System.out.println("Vetor lido, agora vamos realizar a impressão...");
+        for (int i = 0; i < tamanho;i++){
+            System.out.print(vetor[i]+ " ");
+        }
+        sc.close();
+    }
 }
